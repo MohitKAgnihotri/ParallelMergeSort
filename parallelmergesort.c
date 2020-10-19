e1d7ed03f4a0d376a22898d0ec4753855e652439
Mohit Agnihotri <mohit.agnihotri@signify.com>
2020-10-20 00:01:25 +0200

    Added support for merge sort

--- parallelmergesort.c
+++ parallelmergesort.c
@@ -6,11 +6,56 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <limits.h>
+#include <semaphore.h>
 #include "parallelmergesort.h"
 
 signed int *array_to_be_sorted = NULL;
+sem_t arrayReady;
+sem_t firstHalfReady;
+sem_t SecondHalfReady;
+sem_t mergeReady;
 
+void merge(int *A,int p,int q,int r)
+{
+    int n1=q-p+1,n2=r-q;
+    int L[n1+1],R[n2+1],i,j,k;
+    L[n1]=INT_MAX;
+    R[n2]=INT_MAX;
 
+    for (i=0;i<n1;i++)
+        L[i]=A[p+i];
+
+    for (j=0;j<n2;j++)
+        R[j]=A[q+j+1];
+
+    i=0;j=0;
+
+    for (k=p;k<=r;k++)
+    {
+        if(L[i]<=R[j])
+        {
+            A[k]=L[i];
+            i++;
+        }
+        else
+        {
+            A[k]=R[j];
+            j++;
+        }
+    }
+}
+
+void mergeSort(int *A,int p,int r)
+{
+    if (p<r)
+    {
+        int q=(p+r)/2;
+        mergeSort(A,p,q);
+        mergeSort(A,q+1,r);
+        merge(A,p,q,r);
+    }
+}
 
 
 void *createArray(void *args)
@@ -24,26 +69,40 @@ void *createArray(void *args)
         printf("%d \t ", array_to_be_sorted[i]);
     }
     printf("\n");
+    sem_post(&arrayReady); // For the first half
+    sem_post(&arrayReady); // For the second half
 }
 
 
 void *sortSecondHalf(void* args)
 {
-
+    sem_wait(&arrayReady);
+    unsigned int arraysize = *(int*) args;
+    mergeSort(array_to_be_sorted,0,arraysize/2); // Pass starting from 0 to Length - 1 of Array
+    sem_post(&firstHalfReady);
 }
 
 void *sortFirstHalf(void* args)
 {
-
+    sem_wait(&arrayReady);
+    unsigned int arraysize = *(int*) args;
+    mergeSort(array_to_be_sorted,arraysize/2+1,arraysize-1); // Pass starting from 0 to Length - 1 of Array
+    sem_post(&SecondHalfReady);
 }
 
 void *mergeTwoHalves(void* args)
 {
+    sem_wait(&firstHalfReady);
+    sem_wait(&SecondHalfReady);
+
+    printf("Merging halves");
+    sem_post(&mergeReady);
 
 }
 
 void *printSortedArray(void* args)
 {
+    sem_post(&mergeReady);
     unsigned int arraysize = *(int*) args;
     printf("%s", "The element of array have been sorted as follows: \n");
     for (signed int i = 0; i < arraysize; i++)
@@ -66,6 +125,12 @@ int main(int argc, char *argv[])
 
     array_size = atoi(argv[1]);
     srand(time(0));
+
+    sem_init(&arrayReady, 0, 0);
+    sem_init(&firstHalfReady, 0, 0);
+    sem_init(&SecondHalfReady, 0, 0);
+    sem_init(&mergeReady, 0, 0);
+
 
     int rc = pthread_create(&th1, NULL, createArray, (void *)&array_size);
     if (rc){
